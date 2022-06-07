7528431498e8a9bf1401a3dc7047d7057edb99b1
mateuszbaska <mateusz.baska93@gmail.com>
2022-06-07 07:08:57 +0200

    create models & scraper

---
+++ library/books_scraper.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+
+from library.models import Book
+
+products = 1
+url = f"https://www.empik.com/bestsellery/ksiazki?searchCategory=31&hideUnavailable=true&start=1"
+
+r = requests.get(url)
+soup = BeautifulSoup(r.content, "html.parser")
+soup_selector = soup.find("div", class_="search-footer")
+selector = soup_selector.findAll("a")
+last_page = selector[-2]
+page_number = int(last_page.get_text())
+
+for _ in range(page_number - 1):
+    url = f"https://www.empik.com/bestsellery/ksiazki?searchCategory=31&hideUnavailable=true&start={products}"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    soup_selector = soup.findAll("div", class_="search-list-item-hover")
+    for selector in soup_selector:
+        tag_title_book = selector.find("strong", class_="ta-product-title")
+        title_book = tag_title_book.get_text().splitlines()[-1]
+        tag_author_name = selector.find("a", class_="smartAuthor")
+        author_name = tag_author_name.get_text().splitlines()[-1]
+        Book.objects.create(book_title=title_book, author_name=author_name)
+    products += 30
+
---
+++ library/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    e_mail = models.EmailField
+
+
+class Book(models.Model):
+    book_title = models.CharField(max_length=300)
+    author_name = models.CharField(max_length=200)
+
+
+class Borrower(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User)
+    date_of_delivery = models.DateField
+
+
